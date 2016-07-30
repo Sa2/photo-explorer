dc809182e123e629afe8b8fcb71d9aa515fb6eec
Sa2 <ar-ciel_ar-dor_harvestasya@live.jp>
2016-07-31 08:31:49 +0900

    modified not root

--- tools/stop.sh
+++ tools/stop.sh
@@ -2,5 +2,5 @@
 
 PROCESS_ID=`cat /apps/photo-explorer/photo-explorer-app-1.0-SNAPSHOT/RUNNING_PID`
 echo "PID is $PROCESS_ID"
-sudo kill -2 $PROCESS_ID
-sudo rm -rf /apps/photo-explorer/photo-explorer-app-1.0-SNAPSHOT/RUNNING_PID
+kill -2 $PROCESS_ID
+rm -rf /apps/photo-explorer/photo-explorer-app-1.0-SNAPSHOT/RUNNING_PID
