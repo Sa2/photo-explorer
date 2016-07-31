485664a71f5c3320b139e5b1bb0ba1f05434e437
Sa2 <ar-ciel_ar-dor_harvestasya@live.jp>
2016-07-31 09:30:40 +0900

    modified kill -> pkill

--- tools/stop.sh
+++ tools/stop.sh
@@ -1,5 +1,5 @@
 
 PROCESS_ID=`cat /apps/photo-explorer/photo-explorer-app-1.0-SNAPSHOT/RUNNING_PID`
 echo "PID is $PROCESS_ID"
-kill -2 $PROCESS_ID
+pkill $PROCESS_ID
 rm -rf /apps/photo-explorer/photo-explorer-app-1.0-SNAPSHOT/RUNNING_PID
